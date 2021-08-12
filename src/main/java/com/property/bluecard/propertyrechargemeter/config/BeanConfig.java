38fea2cdbb7a63e38991efec831c7c40f848f1c4
weihongzhou <weihongzhou@longfor.com>
2021-08-12 14:57:19 +0800

    端口和数据库版本参数配置

--- src/main/java/com/property/bluecard/propertyrechargemeter/config/BeanConfig.java
+++ src/main/java/com/property/bluecard/propertyrechargemeter/config/BeanConfig.java
@@ -3,16 +3,21 @@ package com.property.bluecard.propertyrechargemeter.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
 
+    @Value("${sqlServerVersion}")
+    private String sqlServerVersion;
+
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.SQL_SERVER2005));
+//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.SQL_SERVER2005));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.getDbType(sqlServerVersion)));
         return interceptor;
     }
 }
