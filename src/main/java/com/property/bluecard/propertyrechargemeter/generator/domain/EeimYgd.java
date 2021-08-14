77777aa14ee05db0383511df099bda2ee18c605f
youqiankeyi <530825969@qq.com>
2021-08-14 14:39:44 +0800

    去除多余字段

--- src/main/java/com/property/bluecard/propertyrechargemeter/generator/domain/EeimYgd.java
+++ src/main/java/com/property/bluecard/propertyrechargemeter/generator/domain/EeimYgd.java
@@ -15,231 +15,61 @@ import java.math.BigDecimal;
 @TableName(value ="EEIM_YGD")
 @Data
 public class EeimYgd implements Serializable {
+
     /**
-     * 
+     *  用户编号
      */
     @TableId
     private String inUserNo;
 
     /**
-     * 
+     * 营业所编号
      */
     private String businessAreaCode;
 
     /**
-     * 
+     *  购电日期
      */
     private String paydate;
 
     /**
-     * 
+     *  月份
      */
     private String mon;
 
     /**
-     * 
+     *  客户姓名
      */
     private String userName;
 
     /**
-     * 
+     *  表号
      */
     private String meterNum;
 
     /**
-     * 
+     *  购电金额
      */
     private BigDecimal paymoney;
 
     /**
-     * 
+     *  购电次数
      */
     private Integer payNum;
 
     /**
-     * 
+     *  标志位
+     *  标志位全部默认为0
+     *  1---------接收数据成功
+     *  2---------接收数据不成功
      */
     private String symbol;
 
-    /**
-     * 
-     */
-    private Short monSn;
 
     /**
-     * 
-     */
-    private String inSUuid;
-
-    /**
-     * 
-     */
-    private String inSRv;
-
-    /**
-     * 
+     *  非必填，系统内的流水号，方便对账用
+     *  售电方保证前台产生的实收的uuid（in_f_uuid）唯一
      */
     private String inFUuid;
 
-    /**
-     * 
-     */
-    private String inCheckNo;
-
-    /**
-     * 
-     */
-    private String inInvoiceNo;
-
-    /**
-     * 
-     */
-    private String inChgOperatorId;
-
-    /**
-     * 
-     */
-    private BigDecimal inMoney;
-
-    /**
-     * 
-     */
-    private String inHandleMode;
-
-    /**
-     * 
-     */
-    private String inReceivedMode;
-
-    /**
-     * 
-     */
-    private String inBalancedMode;
-
-    /**
-     * 
-     */
-    private String inBalancedRemark;
-
-    /**
-     * 
-     */
-    private String inChargedType;
-
-    /**
-     * 
-     */
-    private String outChargedFlag;
-
-    /**
-     * 
-     */
-    private String copyMeterSectNo;
-
-    @TableField(exist = false)
-    private static final long serialVersionUID = 1L;
-
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        EeimYgd other = (EeimYgd) that;
-        return (this.getInUserNo() == null ? other.getInUserNo() == null : this.getInUserNo().equals(other.getInUserNo()))
-            && (this.getBusinessAreaCode() == null ? other.getBusinessAreaCode() == null : this.getBusinessAreaCode().equals(other.getBusinessAreaCode()))
-            && (this.getPaydate() == null ? other.getPaydate() == null : this.getPaydate().equals(other.getPaydate()))
-            && (this.getMon() == null ? other.getMon() == null : this.getMon().equals(other.getMon()))
-            && (this.getUserName() == null ? other.getUserName() == null : this.getUserName().equals(other.getUserName()))
-            && (this.getMeterNum() == null ? other.getMeterNum() == null : this.getMeterNum().equals(other.getMeterNum()))
-            && (this.getPaymoney() == null ? other.getPaymoney() == null : this.getPaymoney().equals(other.getPaymoney()))
-            && (this.getPayNum() == null ? other.getPayNum() == null : this.getPayNum().equals(other.getPayNum()))
-            && (this.getSymbol() == null ? other.getSymbol() == null : this.getSymbol().equals(other.getSymbol()))
-            && (this.getMonSn() == null ? other.getMonSn() == null : this.getMonSn().equals(other.getMonSn()))
-            && (this.getInSUuid() == null ? other.getInSUuid() == null : this.getInSUuid().equals(other.getInSUuid()))
-            && (this.getInSRv() == null ? other.getInSRv() == null : this.getInSRv().equals(other.getInSRv()))
-            && (this.getInFUuid() == null ? other.getInFUuid() == null : this.getInFUuid().equals(other.getInFUuid()))
-            && (this.getInCheckNo() == null ? other.getInCheckNo() == null : this.getInCheckNo().equals(other.getInCheckNo()))
-            && (this.getInInvoiceNo() == null ? other.getInInvoiceNo() == null : this.getInInvoiceNo().equals(other.getInInvoiceNo()))
-            && (this.getInChgOperatorId() == null ? other.getInChgOperatorId() == null : this.getInChgOperatorId().equals(other.getInChgOperatorId()))
-            && (this.getInMoney() == null ? other.getInMoney() == null : this.getInMoney().equals(other.getInMoney()))
-            && (this.getInHandleMode() == null ? other.getInHandleMode() == null : this.getInHandleMode().equals(other.getInHandleMode()))
-            && (this.getInReceivedMode() == null ? other.getInReceivedMode() == null : this.getInReceivedMode().equals(other.getInReceivedMode()))
-            && (this.getInBalancedMode() == null ? other.getInBalancedMode() == null : this.getInBalancedMode().equals(other.getInBalancedMode()))
-            && (this.getInBalancedRemark() == null ? other.getInBalancedRemark() == null : this.getInBalancedRemark().equals(other.getInBalancedRemark()))
-            && (this.getInChargedType() == null ? other.getInChargedType() == null : this.getInChargedType().equals(other.getInChargedType()))
-            && (this.getOutChargedFlag() == null ? other.getOutChargedFlag() == null : this.getOutChargedFlag().equals(other.getOutChargedFlag()))
-            && (this.getCopyMeterSectNo() == null ? other.getCopyMeterSectNo() == null : this.getCopyMeterSectNo().equals(other.getCopyMeterSectNo()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getInUserNo() == null) ? 0 : getInUserNo().hashCode());
-        result = prime * result + ((getBusinessAreaCode() == null) ? 0 : getBusinessAreaCode().hashCode());
-        result = prime * result + ((getPaydate() == null) ? 0 : getPaydate().hashCode());
-        result = prime * result + ((getMon() == null) ? 0 : getMon().hashCode());
-        result = prime * result + ((getUserName() == null) ? 0 : getUserName().hashCode());
-        result = prime * result + ((getMeterNum() == null) ? 0 : getMeterNum().hashCode());
-        result = prime * result + ((getPaymoney() == null) ? 0 : getPaymoney().hashCode());
-        result = prime * result + ((getPayNum() == null) ? 0 : getPayNum().hashCode());
-        result = prime * result + ((getSymbol() == null) ? 0 : getSymbol().hashCode());
-        result = prime * result + ((getMonSn() == null) ? 0 : getMonSn().hashCode());
-        result = prime * result + ((getInSUuid() == null) ? 0 : getInSUuid().hashCode());
-        result = prime * result + ((getInSRv() == null) ? 0 : getInSRv().hashCode());
-        result = prime * result + ((getInFUuid() == null) ? 0 : getInFUuid().hashCode());
-        result = prime * result + ((getInCheckNo() == null) ? 0 : getInCheckNo().hashCode());
-        result = prime * result + ((getInInvoiceNo() == null) ? 0 : getInInvoiceNo().hashCode());
-        result = prime * result + ((getInChgOperatorId() == null) ? 0 : getInChgOperatorId().hashCode());
-        result = prime * result + ((getInMoney() == null) ? 0 : getInMoney().hashCode());
-        result = prime * result + ((getInHandleMode() == null) ? 0 : getInHandleMode().hashCode());
-        result = prime * result + ((getInReceivedMode() == null) ? 0 : getInReceivedMode().hashCode());
-        result = prime * result + ((getInBalancedMode() == null) ? 0 : getInBalancedMode().hashCode());
-        result = prime * result + ((getInBalancedRemark() == null) ? 0 : getInBalancedRemark().hashCode());
-        result = prime * result + ((getInChargedType() == null) ? 0 : getInChargedType().hashCode());
-        result = prime * result + ((getOutChargedFlag() == null) ? 0 : getOutChargedFlag().hashCode());
-        result = prime * result + ((getCopyMeterSectNo() == null) ? 0 : getCopyMeterSectNo().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", inUserNo=").append(inUserNo);
-        sb.append(", businessAreaCode=").append(businessAreaCode);
-        sb.append(", paydate=").append(paydate);
-        sb.append(", mon=").append(mon);
-        sb.append(", userName=").append(userName);
-        sb.append(", meterNum=").append(meterNum);
-        sb.append(", paymoney=").append(paymoney);
-        sb.append(", payNum=").append(payNum);
-        sb.append(", symbol=").append(symbol);
-        sb.append(", monSn=").append(monSn);
-        sb.append(", inSUuid=").append(inSUuid);
-        sb.append(", inSRv=").append(inSRv);
-        sb.append(", inFUuid=").append(inFUuid);
-        sb.append(", inCheckNo=").append(inCheckNo);
-        sb.append(", inInvoiceNo=").append(inInvoiceNo);
-        sb.append(", inChgOperatorId=").append(inChgOperatorId);
-        sb.append(", inMoney=").append(inMoney);
-        sb.append(", inHandleMode=").append(inHandleMode);
-        sb.append(", inReceivedMode=").append(inReceivedMode);
-        sb.append(", inBalancedMode=").append(inBalancedMode);
-        sb.append(", inBalancedRemark=").append(inBalancedRemark);
-        sb.append(", inChargedType=").append(inChargedType);
-        sb.append(", outChargedFlag=").append(outChargedFlag);
-        sb.append(", copyMeterSectNo=").append(copyMeterSectNo);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
